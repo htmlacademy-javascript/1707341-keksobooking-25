99d948111cc1f028e2961ebd7c55571947d2e24c
Narvindjin <Narvindjin@gmail.com>
2022-03-13 13:25:31 +0500

    redo

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
 //Случайное число из положительного диапазона
-const getNumber = function(number1, number2) {
+const getRandomNumber = function(number1, number2) {
   if (!isNaN(number1) && number1 >= 0 && !isNaN(number2) && number2 >= 0) {
-    if (number2 >= number1 && Math.floor(number2) >= Math.ceil (number1)) {
-      // взято из https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      number1 = Math.ceil(number1);
-      number2 = Math.floor(number2);
-      return Math.floor(Math.random() * (number2 - number1 + 1) + number1);
+    const numberBig = Math.floor(Math.max(number1, number2));
+    const numberSmall = Math.ceil(Math.min(number1, number2));
+    if (numberBig < numberSmall) {
+      return null;
     }
-    else if (number2 >= number1 && Math.floor(number2) < Math.ceil (number1)) {
-      return 'Целых чисел в диапазоне нет';
-    }
-    else if (number1 > number2 && Math.floor(number1) >= Math.ceil (number2)) {
-      number2 = Math.ceil(number2);
-      number1 = Math.floor(number1);
-      return Math.floor(Math.random() * (number1 - number2 + 1) + number2);
-    }
-    else if (number1 > number2 && Math.floor(number1) < Math.ceil (number2)) {
-      return 'Целых чисел в диапазоне нет';
-    }
-  } return 'Принимаются только положительные числа';
+    // взято из https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    return Math.floor(Math.random() * (numberBig - numberSmall + 1) + numberSmall);
+  }
+  return null;
 };
 //Случайное число с плавающей точкой из положительного диапазона
-const getNumberFloating = function(number1, number2, floatingLength) {
+const getRandomNumberFloating = function(number1, number2, floatingLength) {
   if (!isNaN(number1) && number1 >= 0 && !isNaN(number2) && number2 >= 0 && !isNaN(floatingLength) && floatingLength >= 0) {
-    if (number2 >= number1) {
-      return (Math.random() * (number2 - number1) + number1).toFixed(floatingLength);
-    }
-    return (Math.random() * (number1 - number2) + number2).toFixed(floatingLength);
-  } return 'Принимаются только положительные числа';
+    const numberBig = Math.max(number1, number2);
+    const numberSmall = Math.min(number1, number2);
+    return Number((Math.random() * (numberBig - numberSmall) + numberSmall).toFixed(floatingLength));
+  } return null;
 };
 
-getNumber (1.5, 2.5);
-getNumberFloating (1.5, 2.5, 3);
+getRandomNumber (1.5, 2.5);
+getRandomNumberFloating (1.5, 2.5, 3);
