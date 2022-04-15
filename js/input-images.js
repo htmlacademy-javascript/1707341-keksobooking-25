8a702c89ee315686740f570bb0f53b2b76898cce
Narvindjin <Narvindjin@gmail.com>
2022-04-15 11:13:37 +0500

    simplify-avatar

--- js/input-images.js
+++ js/input-images.js
@@ -1,27 +1,24 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
-const AVATAR_WIDTH = '40';
-const AVATAR_HEIGHT = '44';
 const DEFAULT_AVATAR = 'img/muffin-grey.svg';
 const PHOTO_PREVIEW_WIDTH = '70';
 const PHOTO_PREVIEW_HEIGHT = '70';
 const avatarInput = document.querySelector('#avatar');
-const avatarContainer = document.querySelector('.ad-form-header__preview');
+const avatarPicture = document.querySelector('.ad-form-header__preview img');
 const photoInput = document.querySelector('#images');
 const photoContainer = document.querySelector('.ad-form__photo');
 
-const addAvatarAttributes = (picture) => {
-  picture.alt = 'Аватар пользователя';
-  picture.width = AVATAR_WIDTH;
-  picture.height = AVATAR_HEIGHT;
+const addAvatarHandler = (input, picture) => {
+  input.addEventListener('change', () => {
+    const file = input.files[0];
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+    if (matches) {
+      picture.src = URL.createObjectURL(file);
+    }
+  });
 };
 
-const addPhotoAttributes = (picture) => {
-  picture.alt = 'Фото пользователя';
-  picture.width = PHOTO_PREVIEW_WIDTH;
-  picture.height = PHOTO_PREVIEW_HEIGHT;
-};
-
-const addPictureHandler = (input, pictureContainer, pictureType) => {
+const addPhotoHandler = (input, pictureContainer) => {
   input.addEventListener('change', () => {
     const file = input.files[0];
     const fileName = file.name.toLowerCase();
@@ -33,34 +30,22 @@ const addPictureHandler = (input, pictureContainer, pictureType) => {
       const picture = document.createElement('img');
       picture.classList.add('picture');
       picture.src = URL.createObjectURL(file);
-      switch (pictureType) {
-        case 'avatar': {
-          addAvatarAttributes(picture);
-          break;
-        }
-        case 'photo': {
-          addPhotoAttributes(picture);
-          break;
-        }
-      }
+      picture.alt = 'Фото пользователя';
+      picture.width = PHOTO_PREVIEW_WIDTH;
+      picture.height = PHOTO_PREVIEW_HEIGHT;
       pictureContainer.appendChild(picture);
     }
   });
 };
 
 const removePictures = () => {
-  avatarContainer.removeChild(avatarContainer.querySelector('.picture'));
-  const avatar = document.createElement('img');
-  avatar.classList.add('picture');
-  avatar.src = DEFAULT_AVATAR;
-  addAvatarAttributes(avatar);
-  avatarContainer.appendChild(avatar);
+  avatarPicture.src = DEFAULT_AVATAR;
   if (photoContainer.querySelector('.picture')){
     photoContainer.removeChild(photoContainer.querySelector('.picture'));
   }
 };
 
-addPictureHandler(avatarInput, avatarContainer, 'avatar');
-addPictureHandler(photoInput, photoContainer, 'photo');
+addAvatarHandler(avatarInput, avatarPicture);
+addPhotoHandler(photoInput, photoContainer);
 
 export {removePictures};
